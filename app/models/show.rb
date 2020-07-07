b353259aa1562e41bc4374e017418288aab305a7
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-07 20:16:25 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,17 @@
 class Show < ActiveRecord::Base
   
-  def 
+  def self.highest_rating
+  end
+
+  def self.most_popular_show
+  end
+  
+  def self.lowest_rating
+  end
+  
+  def self.least_popular_show
+  end
+
+  def self.ratings_sum
+  end
 end
